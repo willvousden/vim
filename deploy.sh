d18bb3998ed8b33f1b1dfdd0c855ddda66c27f5b
Will Vousden <will@vousden.me.uk>
2019-02-28 13:39:00 +0100

    Added Python support reminder to deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -14,3 +14,8 @@ mkdir -p ~/.tmp
 # Install Plug.
 [[ -e "$PLUG_PATH" ]] || curl -fLo "$PLUG_PATH" --create-dirs "$PLUG"
 vim -u "$VIMRC_PATH" +PlugInstall +qall
+
+if hash nvim 2> /dev/null; then
+    echo "Don't forget to set up Python for Neovim!"
+    echo '    $ pip install pynvim'
+fi
