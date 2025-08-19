3216c13339c6d88f24fb9e0a1dc80e302315f52c
Will Vousden <152388+willvousden@users.noreply.github.com>
2025-08-19 20:19:28 +0200

    Improved deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,24 @@ set -euo pipefail
 
 PLUG=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+INIT_PATH=~/.config/nvim/init.vim
 VIM_PATH=$(pwd)
-VIMRC_PATH=$VIM_PATH/vimrc
 PLUG_PATH="$VIM_PATH/autoload/plug.vim"
+PYTHON3_VENV_PATH=~/.python3-neovim
 
 # Deploy dotfiles.
-ln -snf "$VIMRC_PATH" ~/.vimrc
+mkdir -p "$(dirname "$INIT_PATH")"
+ln -snf "$VIM_PATH/init.vim" "$INIT_PATH"
+ln -snf "$VIM_PATH/vimrc" ~/.vimrc
 mkdir -p ~/.tmp
 
 # Install Plug.
 [[ -e "$PLUG_PATH" ]] || curl -fLo "$PLUG_PATH" --create-dirs "$PLUG"
-vim -u "$VIMRC_PATH" +PlugInstall +qall
-
-if hash nvim 2> /dev/null; then
-    echo "Don't forget to set up Python for Neovim!"
-    echo '    $ pip install pynvim'
-fi
+nvim -u "$INIT_PATH" +PlugInstall +qall
 
 ln -sf ../../../../../solarized_custom.vim plugged/lightline.vim/autoload/lightline/colorscheme/solarized_custom.vim
+
+if [[ ! -d $PYTHON3_VENV_PATH ]]; then
+    python3 -m venv "$PYTHON3_VENV_PATH"
+    "$PYTHON3_VENV_PATH/bin/pip3" install -U pip pynvim
+fi
---
+++ init.vim
@@ -0,0 +1,4 @@
+let g:python3_host_prog = '~/.python3-neovim/bin/python3'
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath=&runtimepath
+source ~/git/vim/vimrc
