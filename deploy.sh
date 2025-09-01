d758ed74ca3aa2dabc689a4b75c0cdacc5b11da8
Will Vousden <152388+willvousden@users.noreply.github.com>
2025-09-01 09:54:25 +0200

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,12 @@ mkdir -p ~/.tmp
 [[ -e "$PLUG_PATH" ]] || curl -fLo "$PLUG_PATH" --create-dirs "$PLUG"
 nvim -u "$INIT_PATH" +PlugInstall +qall
 
+# This is a modified Solarized colorscheme for lightline that uses terminal colours.
+# TODO: See if the built-in one can be made to work out-of-the-box (potentially with a GitHub PR).
 ln -sf ../../../../../solarized_custom.vim plugged/lightline.vim/autoload/lightline/colorscheme/solarized_custom.vim
 
+# Make sure the Python venv exists and has the necessary packages.
 if [[ ! -d $PYTHON3_VENV_PATH ]]; then
     python3 -m venv "$PYTHON3_VENV_PATH"
-    "$PYTHON3_VENV_PATH/bin/pip3" install -U pip pynvim
 fi
+"$PYTHON3_VENV_PATH/bin/pip3" install -U pip pynvim
